37978216927f5dcf64391507e304a6da7650dfe9
401asfg <mb.allan@ymail.com>
2024-10-30 18:54:35 -0700

    Initial commit with new empty Unreal Engine 5 project

---
+++ Source/EML_Assignment/EML_Assignment.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EML_Assignment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EML_Assignment, "EML_Assignment" );
